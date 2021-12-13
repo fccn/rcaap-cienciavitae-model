33171b8924887f517ff8c8c8ffa56e3035d78f57
Paulo Graça <paulo1978@gmail.com>
2021-12-13 16:49:29 +0000

    try to address issue with StandardCharsets.UTF_8

--- src/main/java/pt/rcaap/cienciavitae/curriculum/client/SearchRestClient.java
+++ src/main/java/pt/rcaap/cienciavitae/curriculum/client/SearchRestClient.java
@@ -1,6 +1,7 @@
 package pt.rcaap.cienciavitae.curriculum.client;
 
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import javax.xml.bind.JAXBException;
@@ -8,6 +9,7 @@ import pt.cienciavitae.ns.search.Search;
 
 /**
  * Simple search rest client
+ * 
  * @author pgraca
  *
  */
@@ -39,21 +41,26 @@ public class SearchRestClient {
         String[] queryStringPagination = { "page=" + page, "rows=" + rows, "pagination=" + pagination, "order=" + order,
                 "limit=" + limit, "lang=" + lang };
         String path = "/searches/persons";
-        String queryString = path + ((path.indexOf("?") > 0) ? "&query=" : "?query=")
-                + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&" + String.join("&", queryStringPagination);
-
-        InputStream result = null;
-
         try {
-            result = this.connector.get(queryString);
-            if (result != null) {
-                return SearchExtractor.unmarshal(result);
-            }
+            String queryString = path + ((path.indexOf("?") > 0) ? "&query=" : "?query=")
+                    + URLEncoder.encode(query, StandardCharsets.UTF_8.name()) + "&"
+                    + String.join("&", queryStringPagination);
 
-        } catch (JAXBException e) {
-            throw new ClientException("Client jaxb error: " + e.getMessage());
-        } catch (ConnectorException e1) {
-            throw new ClientException("Client connector error for query" + query + "::" + e1.getMessage());
+            InputStream result = null;
+
+            try {
+                result = this.connector.get(queryString);
+                if (result != null) {
+                    return SearchExtractor.unmarshal(result);
+                }
+
+            } catch (JAXBException e) {
+                throw new ClientException("Client jaxb error: " + e.getMessage());
+            } catch (ConnectorException e1) {
+                throw new ClientException("Client connector error for query" + query + "::" + e1.getMessage());
+            }
+        } catch (UnsupportedEncodingException e2) {
+            throw new ClientException("Client URL error for query" + query + "::" + e2.getMessage());
         }
 
         return null;
